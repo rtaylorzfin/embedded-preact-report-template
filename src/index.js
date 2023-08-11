3f6f6bc3a66f39ca39586964017c17db2b3b87a5
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2023-08-11 10:53:57 -0700

    WIP

--- src/index.js
+++ src/index.js
@@ -1,16 +1,60 @@
 import './style.css';
-import { Component, h, render } from 'preact';
 
-class App extends Component {
+// use react
+import React, { useState, render } from 'react';
+import reportData from './report.js';
 
-	render() {
-		return (
-			<div>
-				<p>Embedded Preact template is working!</p>
-			</div>
-		);
-	}
 
+function handleSummaryClick(label) {
+    console.log(label);
+    if (label === 'added') {
+        setShowSequences(!showSequences);
+    }
 }
 
-render(<App />, document.body);
+function ReportItem({ label, value }) {
+    function handleClick(e) {
+        e.preventDefault();
+        console.log(value);
+        handleSummaryClick(label)
+    }
+
+    return <li>
+                <a href="#" onClick={handleClick}>
+                    {label}: {value}
+                </a>
+            </li>
+}
+
+
+function App({ reportData }) {
+    const [showSequences, setShowSequences] = useState(false);
+
+    return <div id="report">
+                    <h1>UniProt Diffs Report</h1>
+                    <h2>Summary</h2>
+                    <ul>
+                        {Object.entries(reportData.summary).map( ([label, value]) =>
+                            <ReportItem label={label} value={value} />
+                        )}
+                    </ul>
+
+                    <div>
+                        <h2>Added Sequences</h2>
+                        <ul>
+                            {reportData.addedSequences.map( seq =>
+                                    <li>${seq.accession}</li>
+                            )}
+                        </ul>
+                    </div>
+                </div>
+          ;
+}
+
+
+function renderApp() {
+    const data = reportData;
+    render(<App reportData={data} />, document.body);
+}
+
+window.addEventListener('DOMContentLoaded', renderApp);
